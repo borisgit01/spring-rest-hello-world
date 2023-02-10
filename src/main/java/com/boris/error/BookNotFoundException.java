4cb90a0bd3b25a5e38c09a1ba22bd9f075138fca
borisgit01 <borisgodunovph@gmail.com>
2023-02-10 10:20:15 -0500

    Initial commit

---
+++ src/main/java/com/boris/error/BookNotFoundException.java
@@ -0,0 +1,7 @@
+package com.boris.error;
+
+public class BookNotFoundException extends RuntimeException {
+    public BookNotFoundException(Long id) {
+        super("Book id not found : " + id);
+    }
+}
---
+++ src/test/java/com/boris/SpringRestHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boris;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringRestHelloWorldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
