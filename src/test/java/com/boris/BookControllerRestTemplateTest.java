d79db306ed42e56160bf1d8fd42c5d8cc71d275c
borisgit01 <borisgodunovph@gmail.com>
2023-02-10 11:53:29 -0500

    Done for now

--- src/test/java/com/boris/BookControllerRestTemplateTest.java
+++ src/test/java/com/boris/BookControllerRestTemplateTest.java
@@ -3,6 +3,7 @@ package com.boris;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONException;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -15,9 +16,11 @@ import org.springframework.http.*;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // for restTemplate
@@ -28,6 +31,35 @@ public class BookControllerRestTemplateTest {
     private TestRestTemplate restTemplate;
     @MockBean
     private BookRepository mockRepository;
+
+    @Before
+    public void init() {
+        Book book = new Book(1L, "A Guide to the Bodhisattva Way of Life", "Santideva", new BigDecimal("15.41"));
+        when(mockRepository.findById(1L)).thenReturn(Optional.of(book));
+    }
+
+    @Test
+    public void find_login_ok() throws Exception {
+        String expected = "{id:1,name:\"A Guide to the Bodhisattva Way of Life\",author:\"Santideva\",price:15.41}";
+        ResponseEntity<String> response = restTemplate
+                .withBasicAuth("user", "password")
+                .getForEntity("/books/1", String.class);
+        printJSON(response);
+        assertEquals(MediaType.APPLICATION_JSON_UTF8, response.getHeaders().getContentType());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        JSONAssert.assertEquals(expected, response.getBody(), false);
+    }
+
+    @Test
+    public void find_nologin_401() throws Exception {
+        String expected = "{\"status\":401,\"error\":\"Unauthorized\",\"message\":\"Unauthorized\",\"path\":\"/books/1\"}";
+        ResponseEntity<String> response = restTemplate
+                .getForEntity("/books/1", String.class);
+        printJSON(response);
+        assertEquals(MediaType.APPLICATION_JSON_UTF8, response.getHeaders().getContentType());
+        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+        JSONAssert.assertEquals(expected, response.getBody(), false);
+    }
 
     @Test
     public void save_emptyAuthor_emptyPrice_400() throws JSONException {
--- src/test/java/com/boris/BookControllerTest.java
+++ src/test/java/com/boris/BookControllerTest.java
@@ -2,6 +2,7 @@ package com.boris;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matcher;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -11,13 +12,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import static org.hamcrest.Matchers.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -28,11 +33,37 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class BookControllerTest {
-    private static final ObjectMapper om = new ObjectMapper();
     @Autowired
     private MockMvc mockMvc;
     @MockBean
     private BookRepository mockRepository;
+
+    @Before
+    public void init() {
+        System.out.println("before");
+        Book book = new Book(1L, "A Guide to the Bodhisattva Way of Life", "Santideva", new BigDecimal("15.41"));
+        when(mockRepository.findById(1L)).thenReturn(Optional.of(book));
+    }
+
+    @WithMockUser("USER")
+    @Test
+    public void find_login_ok() throws Exception {
+        System.out.println("find_login_ok");
+        mockMvc.perform(get("/books/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("A Guide to the Bodhisattva Way of Life")))
+                .andExpect(jsonPath("$.author", is("Santideva")))
+                .andExpect(jsonPath("$.price", is(15.41)));
+    }
+
+    @Test
+    public void find_nologin_401() throws Exception {
+        mockMvc.perform(get("/books/1"))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
 
     @Test
     public void save_EmptyAuthor_EmptyPrice_400() throws Exception {
